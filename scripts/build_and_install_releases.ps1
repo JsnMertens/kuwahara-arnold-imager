bf0c3d3e489d628859f891219609dc1e59399def
Jason Mertens <mertens.jas@gmail.com>
2025-03-13 13:31:48 -0400

    feat: install release within the script

--- scripts/build_and_install_releases.ps1
+++ scripts/build_and_install_releases.ps1
@@ -7,7 +7,7 @@ Write-Host "---------------------------------"
 Write-Host "Building Release for Arnold-7.3.4"
 Write-Host "---------------------------------"
 cmake --preset windows-arnold-7.3.4
-cmake --build . --preset windows-arnold-7.3.4-release
+cmake --build . --target install --preset windows-arnold-7.3.4-release
 if ($LASTEXITCODE -ne 0) {
     Write-Host "Build for windows-arnold-7.3.4-release failed!"
     Pop-Location
@@ -18,7 +18,7 @@ Write-Host "---------------------------------"
 Write-Host "Building Release for Arnold-7.3.5"
 Write-Host "---------------------------------"
 cmake --preset windows-arnold-7.3.5
-cmake --build . --preset windows-arnold-7.3.5-release
+cmake --build . --target install --preset windows-arnold-7.3.5-release
 if ($LASTEXITCODE -ne 0) {
     Write-Host "Build for windows-arnold-7.3.5-release failed!"
     Pop-Location
